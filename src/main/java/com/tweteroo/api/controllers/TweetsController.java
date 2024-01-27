b4d0d428d1cdf50e820d8ce80d5d7b788827ed79
jefti <jeftimeira@gmail.com>
2024-01-27 14:20:41 -0300

    feat: Get tweets by id

--- src/main/java/com/tweteroo/api/controllers/TweetsController.java
+++ src/main/java/com/tweteroo/api/controllers/TweetsController.java
@@ -1,8 +1,10 @@
 package com.tweteroo.api.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,7 +27,13 @@ public class TweetsController {
     }
 
     @GetMapping("/user/{userId}")
-    public String getUserTweets(){
-        return "Em construção!";
+    public Optional<TweetModel> getUserTweets(@PathVariable("userId") Long id){
+        Optional<TweetModel> tweets = tweetRepository.findById(id);
+
+        if(!tweets.isPresent()){
+            return Optional.empty();
+        }
+
+        return Optional.of(tweets.get());
     }
 }
