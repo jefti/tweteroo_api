1b81de46cf192bbe8fac8bc492ad0017c0d52693
jefti <jeftimeira@gmail.com>
2024-01-27 20:21:02 -0300

    fix: Formats problems

--- src/main/java/com/tweteroo/api/dtos/TweetDTO.java
+++ src/main/java/com/tweteroo/api/dtos/TweetDTO.java
@@ -1,15 +1,16 @@
 package com.tweteroo.api.dtos;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class TweetDTO {
-    
+    @NotNull
     @Positive
-    private Long userId;
+    private long userId;
     
     @NotBlank
     @Size(max = 280, message = "O tweet não pode conter mais que 280 caracteres.")
