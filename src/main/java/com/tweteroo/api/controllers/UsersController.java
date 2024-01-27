ed0a69d0985d6c1966d5a40e8057cabd66c94b94
jefti <jeftimeira@gmail.com>
2024-01-27 16:25:18 -0300

    fix: User Service

--- src/main/java/com/tweteroo/api/controllers/UsersController.java
+++ src/main/java/com/tweteroo/api/controllers/UsersController.java
@@ -1,36 +1,31 @@
 package com.tweteroo.api.controllers;
-
-import org.springframework.web.bind.annotation.RestController;
-
 import com.tweteroo.api.dtos.UserDTO;
 import com.tweteroo.api.models.UserModel;
-import com.tweteroo.api.repositories.UserRepository;
+import com.tweteroo.api.services.UserService;
 
-import jakarta.validation.Valid;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/users")
 
 public class UsersController {
     
-    final UserRepository userRepository;
-    UsersController(UserRepository userRepository){
-        this.userRepository = userRepository;
+    final UserService userService;
+    UsersController( UserService userService){
+        this.userService = userService;
     }
 
     @PostMapping
     public ResponseEntity<Object> createUser(@RequestBody @Valid UserDTO body){
-        UserModel user = new UserModel(body);
-        userRepository.save(user);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        UserModel user = userService.save(body);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
 
 }
---
+++ src/main/java/com/tweteroo/api/services/UserService.java
@@ -0,0 +1,32 @@
+package com.tweteroo.api.services;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.tweteroo.api.dtos.UserDTO;
+import com.tweteroo.api.models.UserModel;
+import com.tweteroo.api.repositories.UserRepository;
+
+import jakarta.validation.Valid;
+
+@Service
+public class UserService {
+    final UserRepository userRepository;
+
+    UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+        public ResponseEntity<Object> createUser(@RequestBody @Valid UserDTO body){
+        UserModel user = new UserModel(body);
+        userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    public UserModel save(UserDTO userdto){
+        UserModel user = new UserModel(userdto);
+        return userRepository.save(user);
+    }
+}
